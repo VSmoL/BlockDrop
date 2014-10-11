f8dee74d00fb743d565cbc0d1cf95c679939267d
Timo O <vsmol90@gmail.com>
2014-10-11 18:45:27 +0300

    Enemy spawning and movement

---
+++ BlockDrop/Assets/Game/Enemy/EnemyMovementScirpt.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+// Public variable that contains the speed of the enemy
+public var speed : int = -5;
+
+// Function called when the enemy is created
+function Start () {  
+    // Add a vertical speed to the enemy
+    rigidbody2D.velocity.y = speed;
+
+    // Make the enemy rotate on itself
+    rigidbody2D.angularVelocity = Random.Range(-200, 200);
+
+    // Destroy the enemy in 3 seconds,
+    // when it is no longer visible on the screen
+    Destroy(gameObject, 3);
+}
